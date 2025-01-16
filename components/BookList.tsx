3045ab0ea2b180f8794bbcf0c921a107545bd8a3
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-01-15 21:54:07 -0300

    feat: create Book Components

---
+++ components/BookList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BookList = ({ title, books, containerClassName }) => {
+    return (
+        <section>
+            <h2 className='font-bebas-neue text-4xl text-light-100'>
+                Livros Populares
+            </h2>
+        </section>
+    )
+}
+
+export default BookList
