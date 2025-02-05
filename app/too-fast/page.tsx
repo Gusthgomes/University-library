da743b21e0704fca2a10b4ac2d4f13c948d65e00
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-02-05 17:16:46 -0300

    feat: create too-fast page

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <main className='root-container flex min-h-screen flex-col items-center justify-center'>
+            <h1 className='font-bebas-neue text-5xl font-bold text-light-100'>Uau, devagar aí, rápido!</h1>
+            <p className="mt-3 max-w-xl text-center text-light-400">
+                Parece que você está um pouco ansioso demais. Colocamos um
+                pausa temporária em sua excitação. 🚦 Relaxe um pouco e tente novamente
+                em breve
+            </p>
+        </main>
+    )
+}
+
+export default page
