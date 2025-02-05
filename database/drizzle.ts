ccd2ca282aa81b751b71b1f07b9e5cc4be0b73d1
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-02-05 10:05:28 -0300

    fix: add neon serverless

--- database/drizzle.ts
+++ database/drizzle.ts
@@ -1,6 +1,7 @@
 import config from "@/lib/config";
 import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 
-const sql = config.env.databaseUrl;
+const sql = neon(config.env.databaseUrl);
 
 export const db = drizzle({ client: sql, casing: "snake_case" });
