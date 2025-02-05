8937565bb3e489caec8a053d195315ca6eb87742
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-02-05 16:32:23 -0300

    feat: include signIn and signUp function in the onSubmit method

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -3,13 +3,14 @@
 import React from 'react'
 import AuthForm from '@/components/AuthForm'
 import { signUpSchema } from '@/lib/validations'
+import { signUp } from '@/lib/actions/auth';
 
 const page = () => {
     return (
         <AuthForm type="SIGN_UP"
             schema={signUpSchema}
             defaultValues={{ email: '', password: '', fullName: '', universityId: 0, universityCard: '' }}
-            onSubmit={() => { }}
+            onSubmit={signUp}
         />
     )
 }
