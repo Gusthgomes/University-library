621f9bbfa7c2041b0bfd4445b102eae853a03905
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-02-07 17:10:13 -0300

    fix: correction in the variables names

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { cn } from '@/lib/utils';
-import Image from 'next/image';
 import React from 'react'
 import BookCoverSvg from './BookCoverSgv';
+import { IKImage } from "imagekitio-next";
+import config from "@/lib/config";
 
 type BookCoverVariant = 'extraSmall' | 'small' | 'medium' | 'regular' | 'wide'
 
@@ -29,8 +32,19 @@ const BookCover = ({
     <div className={cn("relative transition-all duration-300", variantStyles[variant], className)}>
       <BookCoverSvg coverColor={coverColor} />
 
-      <div className='absolute z-10' style={{ left: "12%", width: "87,5%", height: "80%" }}>
-        <Image src={coverImage} alt="Book cover" width={400} height={600} className='rounded-sm object-fill' />
+      <div
+        className="absolute z-10"
+        style={{ left: "12%", width: "87.5%", height: "88%" }}
+      >
+        <IKImage
+          path={coverImage}
+          urlEndpoint={config.env.imagekit.urlEndpoint}
+          alt="Book cover"
+          fill
+          className="rounded-sm object-fill"
+          loading="lazy"
+          lqip={{ active: true }}
+        />
       </div>
     </div>
   )
